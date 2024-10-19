b413241c0820f548d36d30a495de8ee9af7bbcd5
hninah <hninah03@gmail.com>
2024-10-19 12:54:15 -0600

    Updated Navigation Bar

--- app/src/main/java/com/example/mini_pekkas/MainActivity.java
+++ app/src/main/java/com/example/mini_pekkas/MainActivity.java
@@ -13,13 +13,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.example.mini_pekkas.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
