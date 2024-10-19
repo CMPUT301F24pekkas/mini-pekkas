7f90b606524d34c657b0225f0fe6555b0edf9fd7
hninah <hninah03@gmail.com>
2024-10-19 12:51:43 -0600

    Updated Navigation Bar

--- app/src/main/java/com/example/mini_pekkas/ui/camera/CameraFragment.java
+++ app/src/main/java/com/example/mini_pekkas/ui/camera/CameraFragment.java
@@ -1,4 +1,4 @@
-package com.example.mini_pekkas.ui.dashboard;
+package com.example.mini_pekkas.ui.camera;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.mini_pekkas.databinding.FragmentDashboardBinding;
+import com.example.mini_pekkas.databinding.FragmentCameraBinding;
+public class CameraFragment extends Fragment {
 
-public class DashboardFragment extends Fragment {
-
-    private FragmentDashboardBinding binding;
+    private FragmentCameraBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        CameraViewModel cameraViewModel =
+                new ViewModelProvider(this).get(CameraViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentCameraBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textCamera;
+        cameraViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
