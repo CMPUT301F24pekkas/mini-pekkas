e9409f6e417ff1dd1bcfbb75bb9ff0610f80fd43
hninah <hninah03@gmail.com>
2024-11-01 16:45:33 -0600

    fixed navigations

--- app/src/main/java/com/example/mini_pekkas/UserActivity.java
+++ app/src/main/java/com/example/mini_pekkas/UserActivity.java
@@ -49,56 +49,5 @@ public class UserActivity extends AppCompatActivity {
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.userView, navController);
-
-
-        /*
-        What follows below are firebase functions I have to test in main because it needs special permissions
-        Clean and remove this calls when you are done testing. They should be called in their respective UI fragment
-        You can also let Android studio compress these into lambda functions for readability. I left them in here for clarity
-         */
-
-        // Initialize Firebase class
-        Firebase firebaseHelper = new Firebase(this);
-
-        // Finds the user document for the given device. Makes a new one if it doesn't exist
-        // Creates a new user Class. See implementation in User.java
-        firebaseHelper.getUser(new Firebase.OnDocumentRetrievedListener() {
-            @Override
-            public void onDocumentRetrieved(DocumentSnapshot documentSnapshot) {
-                User thisUser = new User(documentSnapshot.getData());
-            }
-
-            // All my default error handler just prints the error. Not necessary to implement
-            @Override
-            public void onError(Exception e) {
-                Firebase.OnDocumentRetrievedListener.super.onError(e);
-            }
-        });
-
-        // Finds a list of events in users-in-events collection
-        // Replace eventList with your own event list
-        firebaseHelper.getWaitlist(new Firebase.OnDocumentListRetrievedListener() {
-            @Override
-            public void onDocumentsRetrieved(List<DocumentSnapshot> documentSnapshots) {
-                ArrayList<Event> eventList = new ArrayList<>();
-                for (DocumentSnapshot documentSnapshot : documentSnapshots) {
-                    Event event = new Event(documentSnapshot.getData());
-                    eventList.add(event);
-                };
-            }
-        });
-
-        // Checks if the current user is an admin from the admins collection
-        firebaseHelper.isAdmin(new Firebase.AdminCheckListener() {
-            @Override
-            public void onAdminCheckComplete(boolean isAdmin) {
-                if (isAdmin) {
-                    // User is an admin. Render admin UI
-                } else {
-                    // User is not an admin. Render user UI
-                }
-            }
-        });
     }
-
 }
