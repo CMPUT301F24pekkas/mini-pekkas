de3dfc2138c03890971c5a895f96a06fce971853
dumplingggg <81600369+dumplingggg@users.noreply.github.com>
2024-11-01 18:58:03 -0600

    added add and delete event

--- app/src/main/java/com/example/mini_pekkas/EventArrayAdapter.java
+++ app/src/main/java/com/example/mini_pekkas/EventArrayAdapter.java
@@ -1,2 +1,37 @@
-package com.example.mini_pekkas;public class EventArrayAdapter {
+package com.example.mini_pekkas;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class EventArrayAdapter extends ArrayAdapter<Event> {
+    private ArrayList<Event> events;
+    private Context context;
+    EventArrayAdapter(Context context, ArrayList<Event> events){
+
+        super(context, 0 , events);
+        this.events = events;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+    public void addEvent(Event event){
+        events.add(event);
+        notifyDataSetChanged();
+    }
+    public void deleteEvent(int position){
+        events.remove(position);
+        notifyDataSetChanged();
+    }
+
 }
