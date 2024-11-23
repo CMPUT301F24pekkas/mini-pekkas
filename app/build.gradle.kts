bc8580b6e13b5e1ce262b4ebef6ecc4d76a0b42b
arnoxu1 <arnoxu24@gmail.com>
2024-11-22 19:01:07 -0700

    Added Javadocs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
 
     // Implement android.jar for javadoc, replace with your own filepath
     // implementation(files("/home/ryan/Android/Sdk/platforms/android-34/android.jar"))
-    // implementation(files("C:\\Users\\Arno\\AppData\\Local\\Android\\Sdk\\platforms\\android-34\\android.jar"));
+    implementation(files("C:\\Users\\Arno\\AppData\\Local\\Android\\Sdk\\platforms\\android-34\\android.jar"));
 
 
     // Import the Firebase BoM
